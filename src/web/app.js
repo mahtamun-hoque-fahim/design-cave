336de5f29985272db77d44fafe54613dd7c5c333
Mahtamun Hoque Fahim <mahtamunhoquefahim@gmail.com>
2023-10-11 15:30:18 +0600

    javascript pushed

---
+++ src/web/app.js
@@ -0,0 +1,94 @@
+document.addEventListener("contextmenu", function (fahim) {
+    fahim.preventDefault();
+  });
+  
+  document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+      return false;
+    }
+  };
+  let clickedElement = document.querySelector(".pagenr");
+  clickedElement.addEventListener("click", () => {
+    clickedElement.innerHTML = "Page Not ready!";
+    clickedElement.style.cssText =
+      "display: grid;place-content: center;height: 7vh;width: 13vw;border-radius: 50px;background-color: var(--cb3);color: var(--cw6);font-weight: 400;border: 2.5px transparent;border-radius: 50px;cursor: pointer;width:100%";
+  });
+  
+  
+  // window.onload = function () {
+  //   document.documentElement.classList.add("show-time");
+  // };
+  const primaryNav = document.querySelector(".primary-navigation");
+  const navToggle = document.querySelector(".mobile-nav-toggle");
+  
+  navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+  
+    if (visibility === "false") {
+      primaryNav.setAttribute("data-visible", true);
+      navToggle.setAttribute("aria-expanded", true);
+    } else if (visibility === "true") {
+      primaryNav.setAttribute("data-visible", false);
+      navToggle.setAttribute("aria-expanded", false);
+    }
+  });
+  
+  let builtBy = document.querySelector(".built-by");
+  let codeCounter = document.querySelector(".codeCounter");
+  let heart = document.querySelector(".fa-heart");
+  let blue = document.querySelector(".blue");
+  
+  codeCounter.innerHTML = 478 + 698 + 330 + 47;
+  builtBy.addEventListener("mouseover", () => {
+    codeCounter.style.cssText = "text-shadow:0px 0px 2em var(--cr);";
+    heart.style.cssText = "filter:drop-shadow(0px 0px 1em var(--cr));";
+    blue.style.cssText = "filter:drop-shadow(0px 0px 1em var(--c10lv));";
+  });
+  builtBy.addEventListener("mouseout", () => {
+    codeCounter.style.cssText =
+      "text-shadow:0px 0px 0em var(--cr);filter:drop-shadow(0px 0px 0em var(--c10lv));";
+    heart.style.cssText = "filter:drop-shadow(0px 0px 0em var(--cr));";
+    blue.style.cssText = "filter:drop-shadow(0px 0px 0em var(--c10lv));";
+  });
+  
+  // =-===========  infinite scroll animation  ===================//
+  
+  const scrollers = document.querySelectorAll(".scroller");
+  
+  // If a user hasn't opted in for recuded motion, then we add the animation
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+  }
+  
+  function addAnimation() {
+    scrollers.forEach((scroller) => {
+      // add data-animated="true" to every `.scroller` on the page
+      scroller.setAttribute("data-animated", true);
+  
+      // Make an array from the elements within `.scroller-inner`
+      const scrollerInner = scroller.querySelector(".scroller__inner");
+      const scrollerContent = Array.from(scrollerInner.children);
+  
+      // For each item in the array, clone it
+      // add aria-hidden to it
+      // add it into the `.scroller-inner`
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    });
+  }
+  
